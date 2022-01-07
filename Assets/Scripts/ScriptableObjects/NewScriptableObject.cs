6947d1759981a4c9694e4b37a9f2d4cc59c2a5f5
Lior-Baruch <lior95bar@gmail.com>
2022-01-07 20:41:42 +0200

    first upload to git of mini project
    Mini project -
    Create main menu screen in VR.
    Scene should have the look and feel (UX) of the final escape room,
    and the user should be introduced to the escape room through the
    main menu screen.
    The player should be able to start the escape room via physical or
    remote UI.
    Scene should have a leaderboard which will hold three top users
    and show the time it took them to finish the escape room. (can be
    mock up data for mini project).

---
+++ Assets/Scripts/ScriptableObjects/NewScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScriptableObject : ScriptableObject
+{
+    public string UserName;
+    public float time;
+
+
+}
